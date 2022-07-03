1e8c207f2da7c8a2ed2cd0d665813dd6d2c142d7
LXL <1046787824@qq.com>
2022-07-03 15:21:37 +0800

    20

--- src/main/java/com/liu/CompletableFutureMallDemo.java
+++ src/main/java/com/liu/CompletableFutureMallDemo.java
@@ -1,4 +1,40 @@
 package com.liu;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
 public class CompletableFutureMallDemo {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setId(1);
+        student.setStudentName("LXL");
+        student.setMajor("软件工程");
+
+        //链式编程
+        student.setId(2).setStudentName("123").setMajor("1111111");
+
+        CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> {
+            return "hello";
+        });
+
+//        System.out.println(future.get());
+
+        System.out.println(future.join());
+
+    }
+}
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Accessors(chain = true)
+class Student{
+    private Integer id;
+    private String studentName;
+    private String major;
 }
